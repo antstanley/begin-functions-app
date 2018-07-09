78f210b13b39b0f4ccfdcdd0c7999e635996b5fb
Ant Stanley <antstanley@users.noreply.github.com>
2018-07-09 20:13:36 +0100

    fix response code

--- src/html/get-index/graphiql.js
+++ src/html/get-index/graphiql.js
@@ -4,15 +4,14 @@ function graphiqlFunctions(options, query, callback) {
 
     GraphiQL.resolveGraphiQLString(query, options).then(function (graphiqlString) {
         let result = {
-            status: 200,
-            html: graphiqlString,
+            html: graphiqlString
         };
         callback(null,result);
 
     }, function (error) {
         let result = {
             status: 500,
-            html: error.message,
+            html: error.message
         };
         callback(null,result);
     });
