ba78e95f703fc956f2a2baec44a5f29e1c6ba703
Ant Stanley <antstanley@users.noreply.github.com>
2018-07-09 21:32:04 +0100

    change error status code

--- src/json/post-graphql/graphQLServer.js
+++ src/json/post-graphql/graphQLServer.js
@@ -29,7 +29,7 @@ function graphQLServer (options, request, callback) {
     })
     .catch(function (error) {
       const result = {
-        status: error.statusCode,
+        status: 404,
         json: error.message
       }
 
