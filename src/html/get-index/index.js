a48dfe93e56c61dd2b113dfa25383f979313b449
Ant Stanley <antstanley@users.noreply.github.com>
2018-07-09 19:34:44 +0100

    actually save index.js

--- src/html/get-index/index.js
+++ src/html/get-index/index.js
@@ -2,9 +2,23 @@ let begin = require('@architect/functions')
 
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
-  res({
-    html: 'Hello world!'
-  })
+
+  let options = {
+	    endpointURL: '/api/graphql'
+	}
+
+	server.respondGraphiQLHTML(options, request.query, (err,response)=> {
+	    if (response) {
+	    	res(response)
+	    } else {	    
+		    res({
+		    	status: '404'
+	    		html: "Request can't be fulfilled"
+	  		})
+
+	    }
+	});
+  
 }
 
 exports.handler = begin.html.get(route)
